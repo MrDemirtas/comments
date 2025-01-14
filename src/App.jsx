7d545f538a09769d1a37400a1a8efdcc2bad1971
Furkan Demirtaş <furkan-demirtas@outlook.com>
2025-01-15 00:43:48 +0300

    Tasarım ve get comments işlemleri tamamlandı

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { AttachSvg, BoldSvg, EmojiSvg, ImageSvg, ItalicSvg, LikeSvg, UnderLineSvg } from "./Svg";
+import { AttachSvg, BoldSvg, DissLikeSvg, EmojiSvg, ImageSvg, ItalicSvg, LikeSvg, OrderSvg, ReplySvg, UnderLineSvg } from "./Svg";
 import { useEffect, useState } from "react";
 
 export default function App() {
@@ -15,6 +15,21 @@ export default function App() {
   return (
     <div className="container">
       <AddComment />
+      <hr />
+      <div className="user-comments-content">
+        <div className="user-comments-header">
+          <h3>Comments</h3>
+          <div className="order-comments">
+            <OrderSvg />
+            <select>
+              <option value="Most recent">Most recent</option>
+            </select>
+          </div>
+        </div>
+        {comments.map((x) => (
+          <UserComments key={x.id} {...x} />
+        ))}
+      </div>
     </div>
   );
 }
@@ -51,5 +66,72 @@ function AddComment() {
         <button className="comment-btn">Comment</button>
       </form>
     </div>
+  );
+}
+
+function UserComments({ id, name, time, comment, likes, dislikes, replies }) {
+  return (
+    <ul className="user-comments-body">
+      <li className="comment">
+        <img src="https://avatars.githubusercontent.com/u/178329206?v=4" />
+        <div className="comment-data">
+          <div className="user-data">
+            <strong>{name}</strong>
+            <span>{time}</span>
+          </div>
+          <p className="user-comment">{comment}</p>
+          <div className="comment-interactions">
+            <div className="comment-like-content">
+              <button>
+                <LikeSvg width="18px" /> {likes}
+              </button>
+              <button>
+                <DissLikeSvg width="18px" /> {dislikes}
+              </button>
+            </div>
+            <button>
+              <ReplySvg width="18px" />
+              Reply
+            </button>
+          </div>
+          {replies.length !== 0 && (
+            <ul className="user-comments-body">
+              {replies.map((x) => (
+                <UserCommentReplies key={x.id} {...x} replyToId={id} />
+              ))}
+            </ul>
+          )}
+        </div>
+      </li>
+    </ul>
+  );
+}
+
+function UserCommentReplies({ id, name, time, comment, likes, dislikes, replyToId }) {
+  return (
+    <li className="comment">
+      <img src="https://avatars.githubusercontent.com/u/178329206?v=4" />
+      <div className="comment-data">
+        <div className="user-data">
+          <strong>{name}</strong>
+          <span>{time}</span>
+        </div>
+        <p className="user-comment">{comment}</p>
+        <div className="comment-interactions">
+          <div className="comment-like-content">
+            <button>
+              <LikeSvg width="18px" /> {likes}
+            </button>
+            <button>
+              <DissLikeSvg width="18px" /> {dislikes}
+            </button>
+          </div>
+          <button>
+            <ReplySvg width="18px" />
+            Reply
+          </button>
+        </div>
+      </div>
+    </li>
   );
 }
